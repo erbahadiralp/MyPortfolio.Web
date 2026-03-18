dd97467c9416f262b5c0d7cb180f4a449bf95d5c
erbahadiralp <bahadiralper03@gmail.com>
2026-03-18 16:20:58 +0300

    add CvOriginalFileName migration

---
+++ MyPortfolio.Web/Migrations/20260318132029_AddCvOriginalFileName.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyPortfolio.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCvOriginalFileName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Message",
+                table: "ContactMessages",
+                type: "character varying(2000)",
+                maxLength: 2000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CvOriginalFileName",
+                table: "Abouts",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CvOriginalFileName",
+                table: "Abouts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Message",
+                table: "ContactMessages",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(2000)",
+                oldMaxLength: 2000);
+        }
+    }
+}
